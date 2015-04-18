cb7c1b064da4c69ea3cbc455f6013604ef2fb70c
Shahar Zimmerman <szimmer1@ucsc.edu>
2015-04-17 21:46:59 -0700

    s3 service

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -6,11 +6,58 @@
 
     angular.module('gallery', [])
 
-        .controller('GalleryController', ['$scope', '$stateParams', function($scope, $stateParams) {
+        .factory('awsService', function() {
 
-            $scope.name = $stateParams.name;
-            $scope.test = "This is the GalleryController from the gallery module";
+            if (!AWS) {
+                alert("AWS SDK not found!");
+                return
+            }
 
-        }])
+            function h(direction, input) {
+                if (direction === 'in') {
+                    var hash = "";
+                    _(input.length).times(function(i) {
+                        var n = input.charCodeAt(i);
+                        hash = hash.concat(n);
+                    });
+                    return hash;
+                }
+                else if (direction === 'out') {
+                    var key = "";
+                    _(input.length).times(function(i) {
+                        var n = input.charCodeAt(i);
+                        key = key.concat(n);
+                    });
+                    return key;
+                }
+            }
+
+            return {
+                s3Init: function(id, secret) {
+                    AWS.config.update({
+                        accessKeyId: id,
+                        secretAccessKey: secret
+                    });
+                },
+
+                s3Upload: function(id, secret, files) {
+                },
+
+                getImageUrl: function(files) {
+                }
+            }
+        })
+
+        .controller('GalleryController', ['$scope', '$stateParams', 'awsService',
+            function($scope, $stateParams, awsService) {
+
+                awsService.s3Init(window.s3id, window.s3secret);
+
+                $scope.name = $stateParams.name;
+                $scope.test = "This is the GalleryController from the gallery module";
+
+                $scope.upload = awsService.s3Upload;
+
+            }])
 
 })(angular);
